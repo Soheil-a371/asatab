73c5469455de96fbefedb5f7cb279fcfc194cb5d
Soheil <aliyarsoheil4@gmail.com>
2026-09-02 22:59:29 +0330

    [REFACTOR] not-found style

--- packages/main-app/src/app/not-found.tsx
+++ packages/main-app/src/app/not-found.tsx
@@ -11,15 +11,14 @@ import styles from "./not-found.module.css";
 export default function NotFound(): ReactElement {
   return (
     <div className={styles["not-found"]}>
-      <div className={styles.writing}>
+      <div className={styles.writings}>
         <div className={styles["status-code"]}>404</div>
-        <h1>صفحه مورد نظر پیدا نشد!</h1>
+        <h1>صفحه‌ی مورد نظر پیدا نشد!</h1>
         <p>
-          با عرض پوزش لطفا از طریق جستجو پزشک یا مرکز درمانی مورد نظر خود را
-          جستجو کنید
+          با عرض پوزش، لطفاً از طریق کادر جستجو، پزشک یا مرکز درمانی مورد نظر
+          خود را جستجو کنید.
         </p>
       </div>
-
       <div className={styles.visuals}>
         <Image src={notFoundImage} alt="" />
       </div>
