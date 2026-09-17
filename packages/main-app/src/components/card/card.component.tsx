2d67a75c3066224bbc9ca04dd386dfaef452e3e3
Soheil <aliyarsoheil4@gmail.com>
2026-09-17 17:44:36 +0330

    [UPDATE] theme

---
+++ packages/main-app/src/components/card/card.component.tsx
@@ -0,0 +1,9 @@
+import { PropsWithChildren, ReactElement } from "react";
+
+import styles from "./card.module.css";
+
+type Props = PropsWithChildren;
+
+export default function CardComponent({ children }: Props): ReactElement {
+  return <div className={styles.card}>{children}</div>;
+}
