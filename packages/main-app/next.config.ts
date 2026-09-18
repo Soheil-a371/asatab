ab2895d0d1dc9a1baf43fe454989629b75facad9
Soheil <aliyarsoheil4@gmail.com>
2026-09-18 15:49:47 +0330

    [ADD] implement complete filtering system for doctors search

--- packages/main-app/next.config.ts
+++ packages/main-app/next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.paziresh24.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
+
 export default nextConfig;
+
--- packages/main-app/src/components/global-search-box/global-search-box.component.tsx
+++ packages/main-app/src/components/global-search-box/global-search-box.component.tsx
@@ -1,31 +1,84 @@
-import { ReactElement } from "react";
+"use client";
+
+import {
+  FormEvent,
+  ReactElement,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+
+import { usePathname, useRouter } from "next/navigation";
 
 import MingcuteSearchLine from "@/icons/MingcuteSearchLine";
 import MingcuteLocationLine from "@/icons/MingcuteLocationLine";
 
+
+
 import styles from "./global-search-box.module.css";
+import { FiltersContext } from "@/app/search/provider/filters/filters.provider";
 
 export default function GlobalSearchBoxComponent(): ReactElement {
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const { filters, dispatchFilters } = useContext(FiltersContext);
+
+  const [query, setQuery] = useState<string>("");
+
+  const formSubmitHandler = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (pathname === "/search") {
+      if (query) {
+        dispatchFilters({
+          type: "updated_filter",
+          key: "query",
+          value: query,
+        });
+      } else {
+        dispatchFilters({
+          type: "removed_filter",
+          key: "query",
+        });
+      }
+    } else {
+      const href = query ? `/search/?query=${query}` : "/search";
+      router.push(href);
+    }
+  };
+
+  useEffect(() => {
+    if (pathname !== "/search") {
+      return;
+    }
+
+    const filterQuery = filters.query || "";
+    setQuery(filterQuery);
+
+    const href = filterQuery ? `/search/?query=${filterQuery}` : "/search";
+    router.replace(href);
+  }, [filters, pathname, router]);
+
   return (
-    <div className={styles["global-search-box"]}>
+    <form className={styles["global-search-box"]} onSubmit={formSubmitHandler}>
       <div className={styles.prefix}>
         <MingcuteSearchLine />
       </div>
-
       <input
+        name="query"
         type="text"
         placeholder="نام بیماری، تخصص، پزشک، بیمارستان و ..."
-        aria-label="جستجوی پزشک یا مرکز درمانی"
+        value={query}
+        onChange={(e) => setQuery(e.currentTarget.value)}
       />
-
       <div className={styles.divider}></div>
-
       <div className={styles.suffix}>
-        <button>
+        <button type="button">
           <MingcuteLocationLine />
           همه شهرها
         </button>
       </div>
-    </div>
+    </form>
   );
 }
